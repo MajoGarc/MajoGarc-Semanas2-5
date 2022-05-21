b9102090f7be54418f418e4347469fad3a780b9d
Majo García <321126661@alumno.enp.unam.mx>
2022-05-20 22:03:09 -0500

    tareitas2

--- semana-4/tareitas/dynamics/js/inidex.js
+++ semana-4/tareitas/dynamics/js/inidex.js
@@ -7,48 +7,66 @@ const materia = document.getElementById("materia");
 
 const estatus = document.getElementsByClassName("estatus");
 const borrar = document.getElementsByClassName("borrar");
-var crearinput = document.createElement("input"); 
 
-let materiaS = materia.value, materiaNueva=nueva.value, comp=0, total=0;
+let materiaS = materia.value, comp=0, total=0, nuevaMat=0;
 
 
 materia.addEventListener("change", (evento)=>
 {
     materiaS = materia.value;
-    if(materiaS=="Otra"){   
+    if(materiaS=="Otra")
+    {   
         añadir.style.display = "block";
-        materiaNueva = nueva.value;
-        materia.innerHTML += '<option value="'+materiaNueva+'">'+materiaNueva+'</option>';
-
+        materia.innerHTML += '<option value="'+nueva.value+'">'+nueva.value+'</option>';
+        nuevaMat=1;
     }
 });
 
 
 agregar.addEventListener("click", (evento) => {
-    lista.innerHTML += '<p class="elementos"><button class="estatus">completada</button>'+' '+'<button class="borrar">Borrar</button>'+' '+'<strong>'+ materia.value + '</strong> - '+ tarea.value +' '+'</p>';
-    total++;
-    if(total === 1){
-        pendientes.innerHTML += '<p>'+comp+' tareas completadas de '+total+'</p>'
+    if(tarea.value == "")
+    {
+        alert("el nombre de la tarea es necesario para el registro:)");
+    }
+    else
+    { 
+        if(nuevaMat != 1)
+        {
+            lista.innerHTML += '<p class="elementos"><button class="estatus">completada</button>'+' '+'<button class="borrar">Borrar</button>'+' '+'<strong>'+ materiaS + '</strong> - '+ tarea.value +' '+'</p>';
+            total++;
+            pendientes.innerHTML = '<p>'+comp+' tareas completadas de '+total+'</p>'
+        }
+        else
+        {
+            materiaS=nueva.value;
+            lista.innerHTML += '<p class="elementos"><button class="estatus">completada</button>'+' '+'<button class="borrar">Borrar</button>'+' '+'<strong>'+ materiaS + '</strong> - '+ tarea.value +' '+'</p>';
+            total++;
+            pendientes.innerHTML = '<p>'+comp+' tareas completadas de '+total+'</p>'
+            nuevaMat=0;
+        }
     }
 });
 
 
 lista.addEventListener("click", (evento) => {
-    if(evento.target.className === 'estatus'){
+    if(evento.target.className === 'estatus')
+    {
         if(evento.target.innerHTML=='completada')
-            {
-                comp++;
-                evento.target.innerHTML= 'sin terminar';
-            }
-            else{
-                comp--;
-                evento.target.innerHTML = 'completada';
-            }
-
+        {
+            comp++;
+            evento.target.innerHTML= 'sin terminar';
+            pendientes.innerHTML = '<p>'+comp+' tareas completadas de '+total+'</p>';
+        }
+        else{
+            comp--;
+            evento.target.innerHTML = 'completada';
+            pendientes.innerHTML = '<p>'+comp+' tareas completadas de '+total+'</p>';
+        }
     }
     else if(evento.target.className === 'borrar'){
         evento.target.parentElement.outerHTML = '';
         total--;
-        comp--; 
+    
+        pendientes.innerHTML = '<p>'+comp+' tareas completadas de '+total+'</p>';
     }
 });
