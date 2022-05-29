1e650b890147502b6fbcc8923760ef5296d14f22
Majo García <321126661@alumno.enp.unam.mx>
2022-05-28 23:59:09 -0500

    feat (borrar_pokemon): actualizar archivos 2

--- semana-5/pokedex/dynamics/php/borrar_pokemon.php
+++ semana-5/pokedex/dynamics/php/borrar_pokemon.php
@@ -5,9 +5,28 @@
 
     $id = $_POST['id'];
 
+    // DELETE FROM pokemon_types WHERE pok_id = 6;
+    // DELETE FROM base_stats WHERE pok_id=6;
+    // DELETE FROM pokemon_moves WHERE pok_id=6;
+    // DELETE FROM pokemon_evolution_matchup WHERE pok_id = 6;
+    // DELETE FROM pokemon_abilities WHERE pok_id=6;
+    // DELETE FROM pokemon WHERE pok_id = 6;
+
+
     $sql = "DELETE FROM pokemon_types WHERE pok_id = $id";//elimina primero registros
     mysqli_query($con, $sql);
-    
+
+    $sql = "DELETE FROM base_stats WHERE pok_id = $id";//elimina primero registros
+    mysqli_query($con, $sql);
+
+    $sql = "DELETE FROM pokemon_moves WHERE pok_id = $id";//elimina primero registros
+    mysqli_query($con, $sql);
+
+    $sql = "DELETE FROM pokemon_evolution_matchup WHERE pok_id = $id";//elimina primero registros
+    mysqli_query($con, $sql);
+
+    $sql = "DELETE FROM pokemon_abilities WHERE pok_id = $id";//elimina primero registros
+    mysqli_query($con, $sql);
 
     $sql = "DELETE FROM pokemon WHERE pok_id=$id";//elimina el pokemon
     $res = mysqli_query($con, $sql);
